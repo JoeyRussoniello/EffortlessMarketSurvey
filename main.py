e80cba51b5d6d3852aad95a7ec225aae8a0bcff2
Joseph Russoniello <jmrusso@bu.edu>
2024-10-23 14:01:11 -0400

    Added json config support and ReadME

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from ScraperFunctions.update_chromedriver import update_driver
 from ScraperFunctions.survey import MarketSurvey
+import json
+
+#Get variables from the config file
+with open("config.json", "r") as r:
+    config = json.load(r)
+
+PLATFORM = config['platform']
+HEADLESS = config['headless']
+VERBOSE = config['verbose']
+NCOMPS = config['ncomps']
+INPUT_PATH = config['input_path']
+ROLLUP = config['rollup']
+OUTPUT_PATH = config['output_path']
+OUTPUT_MODE = config['output_mode']
+
 #Update the chrome drivers
-update_driver('win64')
-
-survey = MarketSurvey(headless = True, verbose = True, ncomps = 5) #Initialize Market Survey with parameter inputs
-
-#Perform market survey on the properties and criteria given in input csv
-survey.full_survey(input_path = r"Examples\ExamplePortfolio.csv",rollup = True)
-#survey.full_survey(input_path = "Input.csv",rollup = True)
-#Write the output to output.csv (ONLY APPEND TO COMPLETELY RESET DATA, IF NO FILE APPEND WILL STILL FUNCTION)
-#survey.write_output(mode = "append",file_path = "Output.csv")
-survey.write_output(mode = "append",file_path = r"Examples\PortfolioOutput.csv")
+update_driver(PLATFORM)
+#Perform market survey on the properties and criteria given in input.csv and config.json
+survey = MarketSurvey(headless = HEADLESS, verbose = VERBOSE, ncomps = NCOMPS) #Initialize Market Survey with parameter inputs
+survey.full_survey(input_path = INPUT_PATH,rollup = ROLLUP)
+survey.write_output(mode = OUTPUT_MODE,file_path = OUTPUT_PATH)
