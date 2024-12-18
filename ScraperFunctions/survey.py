59ef801a40f0f150d6773bfc5b2aec7f49fe70c2
Joseph Russoniello <jmrusso@bu.edu>
2024-12-17 19:25:51 -0500

    Updated scraper for new apartments.com HTML

--- ScraperFunctions/survey.py
+++ ScraperFunctions/survey.py
@@ -1,5 +1,12 @@
 #To delete all instances of chromedriver for cleaning is: taskkill /F /IM chromedriver.exe /T
-
+#Standard Imports
+from datetime import datetime
+import re
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import time
+#Pythonic Data Proccessing Imports
+import pandas as pd
+import numpy as np
 #Webscraper selenium Imports
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -7,31 +14,41 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-from selenium.common.exceptions import TimeoutException
-#Pythonic Data Proccessing Imports
-import time
-import pandas as pd
-import numpy as np
-from datetime import datetime
-import re
-from concurrent.futures import ThreadPoolExecutor, as_completed
+from selenium.common.exceptions import TimeoutException,ElementNotInteractableException
 # Get today's date
 today = datetime.today()
 # Format the date as YYYY MM DD
 formatted_date = today.strftime("%m/%d/%Y")
 
 def not_containing(l, filterchar):
+    """Return the list filtered to not contain character filterchar
+
+    Args:
+        l (list): list to filter
+        filterchar (_type_): character to filter out of the list
+
+    Returns:
+        []: Filtered array to not include filter char
+    """
     return list(filter(lambda text: text != filterchar,l))
 def unique(seq):
     #Helper that gets all unique values without modifying order. Used with getLinks
     seen = set()
     seen_add = seen.add
     return [x for x in seq if not (x in seen or seen_add(x))]
+def dollar_to_int(dollar_str):
+    # Remove the dollar sign, extraneous characters and commas
+    clean_str = dollar_str.replace('$', '').replace(',', '').replace("/Person",'')
+    # Convert the cleaned string to an integer
+    try:
+        return int(clean_str)
+    except ValueError:
+        return None
 
 class MarketSurvey():
     def __init__(self,headless = True,verbose = False,ncomps=5):
         service =  Service(executable_path=r".\chromedriver.exe")
-        if headless == False:
+        if not headless:
             self.driver = webdriver.Chrome(service=service)
         else:
             options = Options()
@@ -88,85 +105,104 @@ class MarketSurvey():
     def quit(self):
         self.driver.quit()
     def clean_df(self):
-        self.df.set_index("ID", inplace=True)
+        no_index = self.df.set_index("ID", inplace=False)
 
-        for i in self.df.columns:
-            self.df.loc[self.df[i].apply(lambda i: True if re.search('^\\s*$', str(i)) else False), i] = None #Replace whitespace characters with None values for all columns in the dataframe
+        for i in no_index.columns:
+            no_index.loc[no_index[i].apply(lambda i: True if re.search('^\\s*$', str(i)) else False), i] = None #Replace whitespace characters with None values for all columns in the dataframe
 
-        self.df = self.df[self.df["Pricing"].str[0] == "$"] #Only gets pricing rows that have price values, indicated with dollar signs in Apartments.com
-
+        no_index = no_index[no_index["Pricing"] != "Call for Rent"]
+        #no_index = no_index[no_index["Pricing"].str[0] == "$"] #Only gets pricing rows that have price values, indicated with dollar signs in Apartments.com
         # Security Deposit - Remove extraneous text and attempt convert to float
-        deposit = self.df["Security Deposit"]
-        numified_sd = deposit.apply(pd.to_numeric(lambda x: x.partition(" deposit")[0][1:].replace(",", "") if type(x) == str else x,errors = "coerce"))
-        self.df["Security Deposit"] = numified_sd
+        deposit = no_index["Security Deposit"]
+        numified_sd = pd.to_numeric(
+            deposit.apply(lambda x: x.partition(" deposit")[0][1:].replace(",", "") if type(x) == str else x),
+            errors='coerce'
+        )
+        no_index["Security Deposit"] = numified_sd
 
         # Listed Rent  - Remove comma separators, then convert to float
-        rents = self.df["Pricing"]
-        numified_rent = rents.apply(lambda x: x[1:].replace(",", "")).astype(float)
-        self.df["Pricing"] = numified_rent
+        rents = no_index["Pricing"]
+        new_rents = []
+        for rent in rents:
+            if type(rent) == str:
+                new_str = rent.replace(",","").replace("$","").replace("/Person","").replace("–", "-").strip()
+                if "-" in rent:
+                    min, max = rent.split('-')
+                    new_rents.append((int(min) + int(max))/2)
+                else:
+                    new_rents.append(int(new_str))
+            else:
+                new_rents.append(rent)
+        
+        no_index["Pricing"] = new_rents
+        no_index["Pricing"] = pd.to_numeric(no_index["Pricing"],errors="coerce")
 
         # Square feet - Remove comma separators, then convert to int
-        self.df["Sqft"] = self.df["Sqft"].apply(lambda x: x.replace(",", "")).astype(int)
+        updated_sqft = []
+        for sqft in no_index["Sqft"]:
+            sqft = sqft.replace(",","").replace("–", "-")
+            if "-" in sqft:
+                min,max = sqft.split("-")
+                updated_sqft.append((int(min) + int(max))/2)
+            else:
+                updated_sqft.append(int(sqft))
+        no_index["Sqft"] = updated_sqft
+        no_index["Sqft"] = no_index["Sqft"].astype(int)
 
         # Bed/Baths concatenation and formatting
-        self.df["Bed/Baths"] = self.df["Beds"].astype(str) + " Bed " + self.df["Baths"].apply(
+        no_index["Bed/Baths"] = no_index["Beds"].astype(str) + " Bed " + no_index["Baths"].apply(
             lambda x: int(x) if pd.notnull(x) and isinstance(x, (int, float)) and x % 1 == 0 else x).astype(str) + " Bath"
-    def getCompData(self, property):
+        
+        self.df = no_index
+    def findIndividualUnits(self,rt_prop,competitor):
+        show_more_buttons = self.driver.find_elements(By.CSS_SELECTOR,"button.js-priceGridShowMoreLabel")
+        #Click all of the show more buttons
+        for button in show_more_buttons:
+            try:
+                button.click()
+                WebDriverWait(self.driver, 10).until(
+                    EC.presence_of_all_elements_located((By.CSS_SELECTOR, "li.unitContainer.js-unitContainerV3"))
+                )
+            except ElementNotInteractableException:
+                continue
+        
+        WebDriverWait(self.driver, 10).until(
+            EC.presence_of_all_elements_located((By.CSS_SELECTOR, "li.unitContainer.js-unitContainerV3"))
+        )
         properties = []
-        #Iterate over all links
-        #Wait until page loads
-        try:
-            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "h1.propertyName")))
-        except TimeoutException:
-            return
-        
-        #Find details
-        prop = self.driver.find_element(By.CSS_SELECTOR,"h1.propertyName").text
-        if self.verbose == True:
-            print(f"Gathering data for {prop}...")
-        
-        prop_info = self.driver.find_elements(By.CSS_SELECTOR, "li.unitContainer.js-unitContainer")
+        units = self.driver.find_elements(By.CSS_SELECTOR, "li.unitContainer.js-unitContainerV3")
 
-        divs = self.driver.find_elements(By.CSS_SELECTOR,"div.priceBedRangeInfo")
-
-        #Bro just learned list comprehension
-        plans = [div.find_element(By.CSS_SELECTOR, "span.modelName").text for div in divs]
-        details = [div.find_element(By.CSS_SELECTOR, "h4.detailsLabel").text for div in divs]
-
-
-        #Remove extra blank entries
-        plans = not_containing(plans,"")
-        details = not_containing(details,"")
-
-        #Disgusting list comprehensions to get values from details
-        sqft = []
-        deposits = []
-        for detail in details:
-            if "deposit" in detail:
-                sqft.append("".join(detail.partition("\n")[0].split(",")[2:]).strip())
-                deposits.append(detail.split("\n")[1].partition("deposit")[0] + "deposit")
-            elif "sq ft" in detail:
-                sqft.append("".join(detail.split(",")[2:]).strip())
-                deposits.append("")
+        #Get the depsoit information for each model type and store in a dictionary
+        divs = self.driver.find_elements(By.CSS_SELECTOR,'div.priceBedRangeInfo')
+        modeldict = {}
+        for div in divs:
+            model_name = div.find_element(By.CSS_SELECTOR,"span.modelName").text
+            
+            #If no model name, this is an empty unit - break
+            if model_name == "":
+                break
+            span = div.find_element(By.CSS_SELECTOR,"span.detailsLabel").get_attribute("textContent")
+            if "Deposit" in span:
+                txt = span.partition("Deposit")[0].strip()
+                deposit = txt.split(" ")[-1]
             else:
-                sqft.append("")
-                deposits.append("")
-
-        modeldict = {plans[i]: [sqft[i],deposits[i]] for i in range(len(plans))}
-
+                deposit = ""
+            modeldict[model_name] = deposit
         cols = ["pricing","sqft","available","unit"]
-        for item in prop_info:
+        
+        for item in units:
             data_model = item.get_attribute("data-model")
             if data_model in modeldict:
                 attributes = {
-                    "ID":prop + " " + data_model,
-                    "Area": property,
-                    "Competitor":prop,
+                    "ID":competitor + " " + data_model,
+                    "Area": rt_prop,
+                    "Competitor":competitor,
                     "Beds": item.get_attribute("data-beds"),
                     "Baths": item.get_attribute("data-baths"),
                     "Model": data_model,
-                    "Security Deposit": modeldict[data_model][1],
+                    "Security Deposit": modeldict[data_model],
                     "Report Date":formatted_date,
+                    "Type":"Grid"
                 }
                 for col in cols:
                     nicercol = col[0].upper() + col[1:]
@@ -174,12 +210,82 @@ class MarketSurvey():
                     try:
                         attributes[nicercol] = item.find_element(By.CSS_SELECTOR,f"div.{col}Column.column").text.split("\n")[1]
                     except IndexError:
-                        if (properties == None or properties == []) and self.verbose == True:
-                            print("     No data found")
+                        #If no data - empty unit, break
                         return properties
                 properties.append(attributes)
-        if (properties == None or properties == []) and self.verbose == True:
-            print("     No data found")
+        return properties
+    def findUnitTypes(self,rt_prop,competitor, units):
+        properties = []
+        for unit in units:
+            data_model = unit.find_element(By.CSS_SELECTOR,"span.modelName").text
+            available = unit.find_element(By.CSS_SELECTOR,"span.availabilityInfo").text
+            
+            details = unit.find_element(By.CSS_SELECTOR,'span.detailsTextWrapper')
+            arr = details.get_attribute("innerText").split('\n')
+            beds,baths,sqft = arr[0],arr[1],arr[2]
+            
+            if beds == "Studio":
+                beds = "0 "
+            beds = beds.partition(" ")[0]
+            baths = baths.partition(" ")[0]
+            sqft = sqft.replace(",","").replace("Sq Ft","")
+            
+            price_rng = unit.find_element(By.CSS_SELECTOR,"span.rentLabel").text
+            price_rng = price_rng.replace("/Person","").strip() #Weird corner case for student listings
+            if data_model == "": #Signifies that we have left the valid part of the array
+                break
+            if "–" in price_rng:
+                minimum, maximum = price_rng.split(" – ")
+                minimum, maximum = dollar_to_int(minimum), dollar_to_int(maximum)
+                if minimum != None and maximum != None:
+                    price_rng = (minimum + maximum)/2
+                else:
+                    price_rng = None
+            else:
+                price_rng = dollar_to_int(price_rng)
+            
+            attributes = {
+                    "ID":competitor + " " + data_model,
+                    "Area": rt_prop,
+                    "Competitor":competitor,
+                    "Beds": beds,
+                    "Baths": baths,
+                    "Model": data_model,
+                    "Security Deposit": "",
+                    "Report Date":formatted_date,
+                    "Type": "Gridless",
+                    "Pricing": price_rng,
+                    "Sqft": sqft,
+                    "Available": available,
+                    "Unit": data_model
+                }
+            properties.append(attributes)
+        return properties
+    def getCompData(self, property):
+        properties = []
+        #Iterate over all links
+        #Wait until page loads
+        try:
+            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div.pricingGridItem.multiFamily")))
+        except TimeoutException:
+            return
+        
+        #Find details
+        prop = self.driver.find_element(By.CSS_SELECTOR,"h1.propertyName").text
+
+        with_unit_key = self.driver.find_elements(By.CSS_SELECTOR,"div.pricingGridItem.multiFamily.hasUnitGrid")
+        without_unit_key = self.driver.find_elements(By.CSS_SELECTOR,"div.pricingGridItem.multiFamily")
+
+        if self.verbose == True:
+            print(f"Gathering data for {prop}...")
+        
+        if len(with_unit_key) > 0:
+            properties = self.findIndividualUnits(property,prop)
+        elif len(without_unit_key) > 0:
+            if self.verbose == True:
+                print("Found gridless item")
+            properties = self.findUnitTypes(property,prop,without_unit_key)
+        
         return properties
     def getAreaData(self, area, property,min_price = None,max_price = None):
         #Search for the area on apartments.com
@@ -208,14 +314,48 @@ class MarketSurvey():
                 self.used_links.append(href)
                 numfound += 1
     def rollup_df(self):
-        df_excluded = self.df.drop(columns = ["Beds","Baths"])
-        grouped_mean = df_excluded.groupby(['Area', 'Competitor', 'Bed/Baths', 'Report Date']).mean(numeric_only=True).round(2)
+        # Drop unnecessary columns
+        df_excluded = self.df.drop(columns=["Beds", "Baths"])
+        
+        # Initialize list to collect results
+        result_data = []
 
-        # Group by the same columns to count the number of rows in each group
-        grouped_count = df_excluded.groupby(['Area', 'Competitor', 'Bed/Baths', 'Report Date']).size().reset_index(name='Available Units')
-
-        # Merge the two results: mean values and the count of rows
-        self.df = grouped_mean.merge(grouped_count, on=['Area', 'Competitor', 'Bed/Baths', 'Report Date'])
+        # Iterate over each group and calculate mean and count
+        grouped = df_excluded.groupby(['Area', 'Competitor', 'Bed/Baths', 'Report Date'])
+        for group_keys, group_df in grouped:
+            # Calculate the mean for numeric columns
+            mean_values = group_df.mean(numeric_only=True).round(2)
+            
+            # Check if any row has 'Type' == 'Grid' in the group
+            if (group_df['Type'] == 'Grid').any():
+                available_units_count = (group_df['Type'] == 'Grid').sum()
+            else:
+                available_units_count = None  # Assign pd.NA if no 'Grid' rows exist
+            
+            # Create a dictionary for the row data
+            row_data = {
+                'Area': group_keys[0],
+                'Competitor': group_keys[1],
+                'Bed/Baths': group_keys[2],
+                'Report Date': group_keys[3],
+                'Available_Units_Count': available_units_count,
+                **mean_values.to_dict()
+            }
+            
+            # Append the row data to the results list
+            result_data.append(row_data)
+        
+        # Convert the list of dictionaries to a DataFrame
+        merged_df = pd.DataFrame(result_data)
+        
+        #Reindex 
+        
+        col_list = list(merged_df)
+        #Move Available unit count to the end
+        col_list.append(col_list.pop(4))
+        reindexed = merged_df.reindex(col_list,axis=1)
+        
+        self.df = reindexed
     def full_survey(self,input_path,rollup = True):
         input_df = pd.read_csv(input_path)
         total_props = len(input_df)
@@ -233,7 +373,23 @@ class MarketSurvey():
         #Condense data if asked
         if rollup:
             self.rollup_df()
-    def write_output(self,mode,file_path):
+    def survey_from_links(self,input_path):
+        input_df = pd.read_csv(input_path)
+        for _, row in input_df.iterrows():
+            prop = row['Property']
+            link = row['Link']
+            if link != "":
+                self.driver.get(link)
+                data = self.getCompData(prop)
+                if data != None:
+                    self.data.extend(data)
+        self.df = pd.DataFrame(self.data)
+        self.quit()
+    def modify_df(self,rollup = True):
+        self.clean_df()
+        if rollup:
+            self.rollup_df()
+    def to_csv(self,mode,file_path):
         if mode == 'overwrite':
             write_mode = 'w'  # Overwrite mode
             header = True     # Include the header when overwriting
@@ -245,7 +401,20 @@ class MarketSurvey():
         #Reset index applied to ensure pivot columns included in output
         self.df.reset_index().to_csv(file_path, mode=write_mode, header=header, index=False)
         print(f"Data saved to {file_path}")
+    def to_excel(self, mode, file_path,sheetname):
+        if mode not in ['overwrite', 'append']:
+            raise ValueError("Mode must be 'overwrite' or 'append'")
         
+        if mode == 'overwrite':
+            # Overwrite the file by writing the DataFrame to a new Excel file
+            with pd.ExcelWriter(file_path, engine='openpyxl', mode='w') as writer:
+                self.df.to_excel(writer, index=False, sheet_name=sheetname)
+        elif mode == 'append':
+            with pd.ExcelWriter(file_path, engine='openpyxl', mode='a', if_sheet_exists='overlay') as writer:
+                # Append the new DataFrame to the existing sheet
+                self.df.to_excel(writer, sheet_name=sheetname,index=True, startrow=writer.sheets[sheetname].max_row,header = False)
+        print(f"Data saved to {file_path}")
+
 class MarketSurveyParallel(MarketSurvey):
     @staticmethod
     def process_area_data(row, ncomps, headless=True, verbose=False):
@@ -284,5 +453,5 @@ class MarketSurveyParallel(MarketSurvey):
         if rollup:
             self.rollup_df()
 
-    def write_output(self, mode, file_path):
-        super().write_output(mode, file_path)
+    #def write_output(self, mode, file_path):
+        #super().write_output(mode, file_path)
--- main.py
+++ main.py
@@ -1,11 +1,17 @@
+"""
+Import json to read the `config.json` file
+Import update_driver function, ensuring that web-scraping driver is up to date
+Import Market Survey class to manage the web-scraper
+"""
+import json
 from ScraperFunctions.update_chromedriver import update_driver
 from ScraperFunctions.survey import MarketSurvey, MarketSurveyParallel
-import json
 
-#Get variables from the config file
-with open("config.json", "r") as r:
+#Get a config file from `config.json`
+with open("config.json", "r",encoding='utf-8') as r:
     config = json.load(r)
 
+#Read input variables from the config file
 PLATFORM = config['platform']
 HEADLESS = config['headless']
 VERBOSE = config['verbose']
@@ -18,13 +24,13 @@ PARALLEL = config['parallel_members']
 
 #Update the chrome drivers
 update_driver(PLATFORM)
-if PARALLEL == 0 or PARALLEL == 1:    
+if PARALLEL == 0 or PARALLEL == 1:
     #Perform Market Survey Without Any Parallelism
-    survey = MarketSurvey(headless = HEADLESS, verbose = VERBOSE, ncomps = NCOMPS) #Initialize Market Survey with parameter inputs
+    survey = MarketSurvey(headless = HEADLESS, verbose = VERBOSE, ncomps = NCOMPS)
     survey.full_survey(input_path = INPUT_PATH,rollup = ROLLUP)
-    survey.write_output(mode = OUTPUT_MODE,file_path = OUTPUT_PATH)
+    survey.to_csv(mode = OUTPUT_MODE,file_path = OUTPUT_PATH)
 else:
     #Perform parallel Market Surveys. More Computationally demanding
     survey = MarketSurveyParallel(headless = HEADLESS,verbose = VERBOSE, ncomps = NCOMPS)
     survey.full_survey_parallel(input_path = INPUT_PATH,rollup = ROLLUP,max_workers=PARALLEL)
-    survey.write_output(OUTPUT_MODE,OUTPUT_PATH)
+    survey.to_csv(OUTPUT_MODE,OUTPUT_PATH)
